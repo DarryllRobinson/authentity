922476cb84e4a64b43f65c549bb28a2acbe7ea94
DarryllRobinson <darryll@stillproud.com>
2020-04-13 09:37:33 +1000

    Routes set up

--- src/App.js
+++ src/App.js
@@ -1,26 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Button } from 'react-bootstrap';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  goTo(route) {
+    this.props.history.replace(`/${route}`);
+  }
+
+  login() {
+    this.props.auth.login();
+  }
+
+  logout() {
+    this.props.auth.logout();
+  }
+
+  render() {
+    const { isAuthenticated } = this.props.auth;
+
+    return (
+      <div className="App">
+        <header className="App-header">
+
+
+          {
+            !isAuthenticated() && (
+              <div>
+              <p>
+                Welcome to Authentity!
+              </p>
+                      <Button
+                        onClick={this.login.bind(this)}
+                        style={{ cursor: "pointer" }}
+                      >
+                        Log In
+
+                      </Button>
+              </div>
+          )}
+          {
+            isAuthenticated() && (
+              <div>
+                <Button
+
+                    style={{ cursor: "pointer" }}
+                  >
+                    Process a customer
+                  </Button>
+
+                  <Button
+                    onClick={this.goTo.bind(this, 'profile')}
+                    style={{ cursor: "pointer" }}
+                  >
+                    Profile - coming soon
+                  </Button>
+
+                  <Button
+
+                    style={{ cursor: "pointer" }}
+                  >
+                    How it works - coming soon
+                  </Button>
+
+                  <Button
+                    onClick={this.logout.bind(this)}
+                    style={{ cursor: "pointer" }}
+                  >
+                    Log Out
+                  </Button>
+              </div>
+            )}
+
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
